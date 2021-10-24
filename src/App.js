2a8bf4cd40b769056c95552ed4d910f8e2c46556
berkcatalahmetoglu <45469910+berkcatalahmetoglu@users.noreply.github.com>
2021-10-24 03:32:32 +0300

    Header and SliderSection is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "components/Header";
+import Campaigns from "components/Campaigns";
+import Cards from "components/Cards";
+import Favorites from "components/Favorites";
+import Categories from "components/Categories";
+import Footer from "components/Footer";
+import SliderSection from "components/SliderSection";
+import MobileApp from "components/MobileApp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <SliderSection/>
+      <Campaigns />
+      <Cards />
+      <Favorites />
+      <Categories />
+      <Footer />
+      <MobileApp />
+
+    </>
   );
 }
 
